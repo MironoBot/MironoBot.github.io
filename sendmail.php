02fc7e50fb67e148f43fa6da91002c830ce8f8ee
MironoBot <mironobot@gmail.com>
2023-09-22 00:03:53 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to_email = "mironobot@gmail.com"; // Replace with the recipient's email address
+    $to_email = "recipient@example.com"; // Replace with the recipient's email address
     $subject = "Contact Form Submission";
     $name = $_POST["Name"];
     $email = $_POST["Email"];
@@ -16,18 +16,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if (mail($to_email, $subject, $message_body, $headers)) {
         // Email sent successfully
-        echo '<div class="thankyou_message">
-                <h4><em>Thanks</em> for contacting us! We will get back to you soon!</h4>
-              </div>';
+        echo 'success';
     } else {
         // Error sending email
-        echo '<div class="error_message">
-                <h4>Sorry, there was an error sending your message. Please try again later.</h4>
-              </div>';
+        echo 'error';
     }
 } else {
-    // Redirect back to the form if accessed directly
-    header("Location: your-original-form.html");
-    exit;
+    // Handle other cases if needed
+    echo 'error';
 }
 ?>
